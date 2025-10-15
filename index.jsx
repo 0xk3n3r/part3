5d737f2b30de7b4cc90a7436f9096c7531064dca
k3n3r <Baolan.Shi@colorado.edu>
2025-10-16 00:41:57 +0200

    feat: add morgan middleware for request logging

--- index.jsx
+++ index.jsx
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const repl = require('repl')
+const morgan = require('morgan')
 
+app.use(morgan('tiny'));
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
