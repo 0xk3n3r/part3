7b0f08bf2ae096ae2ae639d60fe05f2853e9b277
k3n3r <Baolan.Shi@colorado.edu>
2025-10-18 23:16:30 +0200

    refactor: streamline person creation logic and improve argument handling

--- mongo.js
+++ mongo.js
@@ -19,10 +19,6 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const name = process.argv[3]
-const number = process.argv[4]
-const person = new Person({ name, number })
-
 /*
 Note.find({}).then(result => {
   result.forEach(note => {
@@ -31,8 +27,34 @@ Note.find({}).then(result => {
   mongoose.connection.close()
 })
 */
+if (process.argv.length === 3) {
+      Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      }).catch(error => {
+        console.error('Error fetching data:', error)
+        mongoose.connection.close()
+      })
+    } else if (process.argv.length === 5) {
+      const name = process.argv[3]
+      const number = process.argv[4]
+      const person = new Person({ name, number })
 
-person.save().then(result => {
-  console.log(`Added ${name} number ${number} to phonebook`)
-  mongoose.connection.close()
-})
+      person.save()
+        .then(() => {
+          console.log(`Added ${name} number ${number} to the phonebook`)
+        })
+        .catch(error => {
+          console.error('Error saving person:', error)
+        })
+        .finally(() => {
+          mongoose.connection.close()
+        })
+    } else {
+      console.log('Invalid arguments')
+      mongoose.connection.close()
+    }
+
