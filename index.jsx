dac46709dafd9babb160d62c91a668f6c2337dd3
k3n3r <Baolan.Shi@colorado.edu>
2025-10-19 00:33:13 +0200

    feat: update person retrieval logic to use MongoDB and handle errors

--- index.jsx
+++ index.jsx
@@ -70,14 +70,18 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -117,7 +121,7 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number,
   })
-  
+
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
