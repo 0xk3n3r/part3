43e2804cfda4f1e579906f5597c1f68a9be83254
k3n3r <Baolan.Shi@colorado.edu>
2025-10-18 21:30:34 +0200

    feat: update package dependencies and add mongo.js for database connection

---
+++ mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const url =
+  `mongodb+srv://adminer:911225@cluster0.zffjt2e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
