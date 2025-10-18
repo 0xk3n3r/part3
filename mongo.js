ee56a674c796f0e4fdd0113b494d0ee553d46488
k3n3r <Baolan.Shi@colorado.edu>
2025-10-18 22:44:27 +0200

    feat: update MongoDB connection to use dynamic password and modify schema for person data

--- mongo.js
+++ mongo.js
@@ -1,25 +1,39 @@
 const mongoose = require('mongoose')
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
+const password = process.argv[2]
 const url =
-  `mongodb+srv://adminer:911225@cluster0.zffjt2e.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://adminer:${password}@cluster0.zffjt2e.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
+const person = new Person({
+    name: "Arto Hellas",
+    number: "040-123456"
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+/*
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+*/
+
+person.save().then(result => {
+  console.log('person saved!')
   mongoose.connection.close()
 })
